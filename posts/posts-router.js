f716bdb06241a2bd54d310ea12d53312eb068f23
teej387 <teej387@yahoo.com>
2019-08-09 22:08:09 -0500

    Created router.post handler

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,3 +2,21 @@ const express = require('express')
 const db = require('../data/db.js')
 const router = express.Router()
 
+router.post('/', async (req, res) => {
+  const { title, contents } = req.body
+
+  if (!title || !contents) {
+    res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' })
+  }else {
+    try {
+      const hub = await db.insert(req.body)
+      res.status(201).json(hub)
+    }catch {
+      res.status(500).json({
+        error: 'There was an error while saving the post to the database'
+      })
+    }
+  }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
-const db = require('./data/db.js')
+
+const PostsRouter = require('./posts/posts-router.js')
+
 const server = express()
 
 server.use(express.json())
+server.use('/api/posts', PostsRouter)
+
+server.get('/', (req, res) => {
+  res.send('Howdy!')
+})
 
 module.exports = server
